6f150afc21cd3487e93b444e9b42cab3556dc7d9
larryjuma <larryjuma198@gmail.com>
2025-05-07 04:56:16 +0300

    updated the whole page

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const year = document.getElementById("fullYear")
+document.addEventListener("DOMContentLoaded", () => {
+  const currtime = document.querySelector(".curr-time");
+  const greeting = document.querySelector(".greetings");
 
-function fullYear(){
-    let now = new Date();
-    let currentYear = now.getFullYear()
-    year.innerText = currentYear;
-}
+  function time() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let year = now.getFullYear();
 
-fullYear()
-    
+    document.getElementById("fullYear").innerHTML = `${year}`
 
+    const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+    currtime.innerText = formattedTime;
+
+    let greetMsg;
+    if (hours < 12) {
+      greetMsg = "Good Morning!";
+    } else if (hours >=12 && hours < 16) {
+      greetMsg = "Good Afternoon!";
+    }
+     else if(hours >= 16 && hours < 20) {
+      greetMsg = "Good Evening!";
+    }
+    else{
+      greetMsg = "Good Night!";
+    }
+    greeting.innerText = greetMsg;
+  }
+
+  time();
+  setInterval(time, 60000);
+});
 
